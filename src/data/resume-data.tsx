e83cb5b47985e7b512437f964f0ddd8c47c395a4
Zhipeng yin <zhip_y@yeah.net>
2024-06-17 21:00:29 -0500

    update github url

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -50,6 +50,12 @@ export const RESUME_DATA = {
   },
   education: [
     {
+      school: "Florida International University",
+      degree: "Doctoral Degree in Computer Science",
+      start: "2023",
+      end: "Present",
+    },
+    {
       school: "Stevens Institute of Technology",
       degree: "Master's Degree in Computer Science",
       start: "2021",
