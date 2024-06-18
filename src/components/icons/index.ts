a1cc73edfad5d252924dfc160dd82bc0424b5063
Zhipeng yin <zhip_y@yeah.net>
2024-06-18 01:09:38 -0500

    scholar

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,7 @@
 import { GitHubIcon } from "./GitHubIcon";
 import { LinkedInIcon } from "./LinkedInIcon";
 import { XIcon } from "./XIcon";
+// @ts-ignore
+import {Scholar} from './google-scholar.png'
 
-export { GitHubIcon, LinkedInIcon, XIcon };
+export { GitHubIcon, LinkedInIcon, XIcon,Scholar };
